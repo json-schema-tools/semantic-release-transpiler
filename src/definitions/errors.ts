62bb68ed20fa0d6392aec62e9f8fba7af07e9aff
Zachary Belford <belfordz66@gmail.com>
2020-04-03 15:52:29 -0700

    fix: update random garb

--- src/definitions/errors.ts
+++ src/definitions/errors.ts
@@ -2,10 +2,9 @@
 export default {
   ENODOCUMENT: () => ({
     code: "ENODOCUMENT",
-    message: "Missing `openrpc.json` document file.",
+    message: "Missing json schema document file.",
     details: [
-      "An [OpenRPC Document](https://spec.open-rpc.org/#openrpc-document) at the root of your project or specified in the config is required. ", //tslint:disable-line
-      "Please follow the [OpenRPC Specification](http://spec.open-rpc.org) to create a valid `openrpc.json` file.",
+      "you must have a json schema to run this plugin"
     ].join("\n\n"),
   }),
 };
