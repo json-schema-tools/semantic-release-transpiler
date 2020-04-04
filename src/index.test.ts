45d6ce6e740eed22fef1f0ce2565be67cfbcf553
Zachary Belford <belfordz66@gmail.com>
2020-04-03 17:25:23 -0700

    fix: significant chagnes

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,21 +4,20 @@ import * as fs from "fs";
 import * as path from "path";
 import * as util from "util";
 
-const appendFile = util.promisify(fs.appendFile);
 const readFile = util.promisify(fs.readFile);
 
-const p = path.resolve(process.cwd(), "./openrpc.json");
 
+describe("json-schema-tools semantic-release plugin", () => {
+  afterEach(() => fs.rmdirSync("./testeroo", { recursive: true }));
 
-describe("openrpc plugin", () => {
   describe("verifyConditions", () => {
     it("can error on verifyConditions", () => {
-      return verifyConditions({ schemaLocation: "./src/test.json", languages: { ts: true } }, {}).catch((e: SemanticReleaseError) => {
+      return verifyConditions({ outpath: "./testeroo", schemaLocation: "./src/test.json", languages: { ts: true } }, {}).catch((e: SemanticReleaseError) => {
         expect(e.message).toContain("Missing json schema document file.");
       });
     });
     it("can pass verifyConditions", () => {
-      return verifyConditions({ schemaLocation: "./src/test-schema.json", languages: { ts: true } }, {}).then((valid: boolean) => {
+      return verifyConditions({ outpath: "./testeroo", schemaLocation: "./src/test-schema.json", languages: { ts: true } }, {}).then((valid: boolean) => {
         expect(valid).toEqual(true);
       });
     });
@@ -26,16 +25,15 @@ describe("openrpc plugin", () => {
 
   describe("prepare", () => {
     it("can fail if no next release version", () => {
-      return prepare({ schemaLocation: "./src/test-schema.json", languages: { ts: true } }, {}).catch((e: SemanticReleaseError) => {
+      return prepare({ outpath: "./testeroo", schemaLocation: "./src/test-schema.json", languages: { ts: true } }, {}).catch((e: SemanticReleaseError) => {
         expect(e.message).toContain("No nextRelease version");
       });
     });
     it("can pass prepare and set the version", async () => {
-      return prepare({ schemaLocation: "./src/test-schema.json", languages: { ts: true } }, { nextRelease: { version: "1.0.0" } })
+      return prepare({ outpath: "./testeroo", schemaLocation: "./src/test-schema.json", languages: { ts: true } }, { nextRelease: { version: "1.0.0" } })
         .then(async (prepared: boolean) => {
-          const file = await readFile("./generated-typings.ts", "utf8");
+          const file = await readFile("./testeroo/src/generated-typings.ts", "utf8");
           expect(typeof file).toEqual("string");
-          fs.unlinkSync("./generated-typings.ts");
         });
     });
   });
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,19 @@ import checkExists from "./checkExists";
 import SemanticReleaseError from "./semanticReleaseError";
 import errors from "./definitions/errors";
 import JsonSchemaToTypes from "@etclabscore/json-schema-to-types";
+import { camelCase } from "lodash";
 import * as fs from "fs";
 import { promisify } from "util";
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
+const cp = promisify(fs.copyFile);
+const mkdir = promisify(fs.mkdir);
 
+const tsc = require("node-typescript-compiler");//eslint-disable-line
 
 interface PluginConfig {
-  schemaLocation: string | string[];
-  outputName?: string;
+  outpath: string;
+  schemaLocation: string;
   languages?: {
     ts?: boolean;
     go?: boolean;
@@ -32,29 +36,18 @@ let verified = false;
 
 export const verifyConditions: PluginFunction = async (pluginConfig, context): Promise<boolean> => {
   const cwd = process.cwd();
-  const config: PluginConfig = {
-    schemaLocation: pluginConfig.schemaLocation,
-  };
 
-  const paths = [];
-  if (config.schemaLocation === undefined) {
+  if (pluginConfig.schemaLocation === undefined) {
     throw new SemanticReleaseError("You must provide a schema location", "123321", "json-schema-tools transpiler requires a schema");
-  } else if (config.schemaLocation instanceof Array) {
-    config.schemaLocation.forEach((loc) => paths.push(path.resolve(cwd, loc)))
-  } else {
-    paths.push(path.resolve(cwd, config.schemaLocation))
   }
 
-  const existsPromises = paths.map(async (p) => {
-    const exists = await checkExists(p);
-    if (!exists) {
-      const noDocumentError = errors.ENODOCUMENT();
-      throw new SemanticReleaseError(noDocumentError.message, noDocumentError.code, noDocumentError.details);
-    }
-    return exists;
-  });
+  const sPath = path.resolve(cwd, pluginConfig.schemaLocation);
 
-  await Promise.all(existsPromises);
+  const exists = await checkExists(sPath);
+  if (!exists) {
+    const noDocumentError = errors.ENODOCUMENT();
+    throw new SemanticReleaseError(noDocumentError.message, noDocumentError.code, noDocumentError.details);
+  }
 
   verified = true;
   return verified;
@@ -68,19 +61,44 @@ export const prepare: PluginFunction = async (pluginConfig, context): Promise<bo
     throw new SemanticReleaseError("No nextRelease version", "ENOVERSION", "Something went wrong and there is no next release version"); //tslint:disable-line
   }
 
-  const cwd = process.cwd();
-  const schemas = [];
-  if (pluginConfig.schemaLocation instanceof Array) {
-    pluginConfig.schemaLocation.forEach(async (loc) => schemas.push(JSON.parse(await readFile(path.resolve(cwd, loc), "utf8"))));
-  } else {
-    schemas.push(JSON.parse(await readFile(path.resolve(cwd, pluginConfig.schemaLocation), "utf8")))
+  const outpath = pluginConfig.outpath || process.cwd();
+  await mkdir(`./${outpath}/src`, { recursive: true });
+
+  const schemaPath = path.resolve(process.cwd(), pluginConfig.schemaLocation);
+  await cp(schemaPath, `${outpath}/src/schema.json`);
+
+  const schema = JSON.parse(await readFile(schemaPath, "utf8"));
+
+  if (!schema.title) {
+    throw new SemanticReleaseError("The schema must have a title", "ENOTITLE", "Schema requires a title");
   }
 
-  const transpiler = new JsonSchemaToTypes(schemas);
-  const outTS = path.resolve(cwd, pluginConfig.outputName || "generated-typings");
+  const transpiler = new JsonSchemaToTypes(schema);
+  const outTS = path.resolve(outpath, "src/generated-typings");
 
   if (!pluginConfig.languages || pluginConfig.languages.ts) {
     await writeFile(`${outTS}.ts`, transpiler.toTs());
+
+    const indexTS = path.resolve(outpath, "src/index.ts");
+    const regularName = camelCase(schema.title);
+    const ts = [
+      `import * as Generated from "./generated-typings";`,
+      `import ${regularName} from "${schemaPath}"`,
+      `export default { ${regularName}, ...Generated };`
+    ].join("\n");
+    await writeFile(indexTS, ts)
+    await tsc.compile({
+      "target": "es6",
+      "module": "commonjs",
+      "lib": [
+        "es2015",
+      ],
+      "declaration": true,
+      "outDir": "./build",
+      "strict": true,
+      "esModuleInterop": true,
+      "resolveJsonModule": true
+    });
   }
   if (!pluginConfig.languages || pluginConfig.languages.go) {
     await writeFile(`${outTS}.go`, transpiler.toGo());
